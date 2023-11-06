d07e8c25d350e71bbbe123e756d8cb38aded14f9
alek <wait82985@gmail.com>
2023-11-06 14:59:09 +0600

    problem solved

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -8,9 +8,9 @@ public class Node {
         val = 0;
         neighbors = new ArrayList<Node>();
     }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
+    public Node(int val) {
+        this.val = val;
+        this.neighbors = new ArrayList<Node>();
     }
     public Node(int val, ArrayList<Node> neighbors) {
         this.val = val;
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
     public Node cloneGraph(Node node) {
-
+        Map<Node, Node> map = new HashMap<>();
+        traversAndMap(map, node);
+        for (Map.Entry<Node, Node> entry: map.entrySet()) {
+            Node original = entry.getKey();
+            Node clone = entry.getValue();
+            for (int i = 0; i < original.neighbors.size(); i++) {
+                clone.neighbors.add(map.get(original.neighbors.get(i)));
+            }
+        }
+        return map.get(node);
     }
+
+
+    private void traversAndMap(Map<Node, Node> map, Node root){
+        if(root == null){
+            return;
+        }
+        map.put(root, new Node(root.val));
+
+        for (int i = 0; i < root.neighbors.size(); i++) {
+            if(!map.containsKey(root.neighbors.get(i))){
+                traversAndMap(map, root.neighbors.get(i));
+            }
+        }
+    }
+
 }
